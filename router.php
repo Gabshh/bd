8a61612607eb37c2484ebfd9207d383fb501e09f
GABRIEL GOMES SAVIO <21276337@CFP127ALUNO.LOCAL>
2022-03-04 16:51:28 -0300

    adicionado arquivo router.php

---
+++ router.php
@@ -0,0 +1,33 @@
+<?php 
+
+    /*************************************************************************************
+     * Objetivo: Arquivo de rota, para segmentar as ações encaminhadas pela view
+     *      (dados de um form, listagem de dados, uma ação de excluir ou atualizar)
+     *      Esse arquivo será responsável por encaminhar as solicitações para a Controller
+     * Dev: Gabriel Gomes
+     * Versão: 1.0
+     * Data: 04/03/2022
+     ************************************************************************************/
+
+    $action = (string) null;
+    $component = (string) null;
+
+    //Validação para verificar se a requisição é umPOST de um formulário
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        //Recebendo dados via URL para saber quem esta solicitando e qual ação será 
+        //realizada
+        $component = strtoupper($_GET['component']);
+        $action = $_GET['action'];
+
+        //Estrutura condicional para validar quem esta solicitando algo para o Router
+        switch($component){
+            case 'CONTATOS':
+                echo('chamando a controller de contatos');
+                break; 
+        }
+
+        
+    }
+
+?>
